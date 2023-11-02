ce9931a8235132aea3ad6210b5cf57117f706d73
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-11-02 16:46:41 -0700

    Initial Push
    Creating some starter codes

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+      mode: 'development',
+      entry: { // Entry point for files
+          main: './src/js/index.js',
+          install: './src/js/install.js',
+          // editor: './src/editor.js'
+      },
+      output: { // Output for our bundles
+          filename: '[name].bundle.js',
+          path: path.resolve(__dirname, 'dist'),
+      },
+      plugins: [
+          new HtmlWebpackPlugin({ // Webpack plugin that generates our html file and injects our bundles. 
+              template: './index.html',
+              title: 'JATE'
+          }),
+          new InjectManifest({ // Injects our custom service worker
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js'
+          }),
+          new WebpackPwaManifest({ // Creates a manifest.json file.
+              fingerprints: false,
+              inject: true,
+              name: 'Just Another Text Editor',
+              short_name: 'JATE',
+              description: 'Create notes and code snippets with or without an internet connection.',
+              background_color: '#f5f5f5',
+              theme_color: '#31a9e1',
+              start_url: './',
+              publicPath: './',
+              icons: [
+                  {
+                      src: path.resolve('src/images/logo.png'),
+                      sizes: 96,
+                      destination: path.join('assets', 'icons'),
+                  }
+              ],
+          }),
+      ],
+
+      module: {
+          rules: [ // CSS loaders
+              {
+                  test: /\.css$/i,
+                  use: ['style-loader', 'css-loader'],
+              },
+              {
+                  test: /\.m?js$/,
+                  exclude: /node_modules/,
+                  use: {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: ['@babel/preset-env'],
+                          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                      },
+                  },
+              },
+          ],
+      },
+  };
+};
---
+++ README.md
@@ -0,0 +1,60 @@
+# Hyperlitz Just Another Text Editor 
+
+## Description
+
+Just Another Text Editor (J.A.T.E) is a Progressive Web Application (PWA) that runs in the browser, offline and can be installed locally to your machine. This application features a number of data persistence techniques, insuring the application runs regardless of browser supported function. J.A.T.E uses an IndexedDB database and the idb package. This application is deployed to Heroku, to access it in production continue reading the documentation!
+
+## Table of Contents
+1. [Description](#description)
+2. [Table of Contents](#table-of-contents)
+4. [Installation](#installation)
+5. [License](#license)
+6. [Technologies Employed](#technologies-employed)
+7. [Future Development](#future-development)
+8. [Contributing](#contributing)
+9. [Tests](#tests)
+10. [Questions](#questions)
+
+
+## Installation
+This application is deployed to [Heroku]().
+
+To run J.A.T.E locally:
+
+1. Pull down and/or branch this repository
+2. Run ```npm i``` to install all dependencies
+3. Invoke application with ```npm run start```
+</br>
+
+## License
+This project is licensed under the MIT license.
+
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.<p/>For more information visit [MIT Licensing](https://choosealicense.com/licenses/mit/).
+
+## Technologies Employed
+* Mini-CSS-Extract Plugin
+* Webpack+Workbox
+* Concurrently
+* JavaScript
+* IndexedDB
+* Express
+* NodeJS
+* Babel
+
+
+## Future Development
+We would like to continue to add the following functionality to our application:
+- Overhaul CSS
+
+## Contributing
+We'd love for you to contribute! In order to do so, fork this repository. Your pull request will need approval in order to merge to ```main```. <br/><br/> Take a look at our [Future Development](#future-development) section to see what we are looking to expand on (implemented features are denoted with a &check;). Feel free to implement your own ideas and merge request!
+
+## Tests
+No tests were run to complete this CMS.
+
+## Questions
+Lito Banano[GitHub](https://github.com/hyperlitz)<br/>
+Or visit the PWA's repository: [J.A.T.E]()
+
+- - -
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
